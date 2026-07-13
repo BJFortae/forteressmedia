51ecc031b095d4a1064567916aa2a38f1c891ed2
brendenforte <brenden.forte@gmail.com>
2026-07-12 19:10:44 -0500

    cleaned up folder structure and tightened prototype

--- app/preview/proscapes/[...path]/route.js
+++
@@ -1,6 +0,0 @@
-import { serveProscapesFile } from "../../../lib/proscapes-static";
-
-export async function GET(_request, { params }) {
-  const { path = [] } = await params;
-  return serveProscapesFile(path);
-}
--- app/preview/proscapes/route.js
+++
@@ -1,5 +0,0 @@
-import { proscapesPitchRedirect } from "../../lib/proscapes-static";
-
-export function GET(request) {
-  return proscapesPitchRedirect(request);
-}
